d5c7ccc5ed2ad5c2d1dffe60fb9b5c641d1fd6aa
Minilool <111468388+NyKepler@users.noreply.github.com>
2024-02-02 16:49:18 +0100

    Add files via upload

---
+++ SpaceRanger/3_SR_count.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#' connect server through ssh Kepler78@130.235.5.221 -p 1220
+home=$HOME
+path=$home/OVisium/Spaceranger
+input=$path/input_rerun.csv
+cd $path/Outs
+
+#' Perform sitecheck
+export PATH=$path/spaceranger-2.0.1:$PATH
+spaceranger sitecheck > sitecheck.txt
+
+#' A sample sheet contains 7 columns 
+while IFS=, read -r patient mutation variant age sample origin library slide area image run fastq json
+do
+
+if [[ "$json" == "NA" ]]
+then
+#' Extact count on autodetected area   
+#' Jason file is generated automatically based on the provided image   
+#' Image should be in either TIFF, QPTIFF or JPEG format
+#' The following step is done by the CTG facility after the sequencing usinbg 
+#' version 1.2.2               
+spaceranger count --id=$library --transcriptome=$path/refdata-gex-GRCh38-2020-A --fastqs=$path/$run/fastq/$fastq --sample=$fastq --image=$path/$run/image/$image --slide=$slide --area=$area --localcores=8 --localmem=500 --no-bam --r1-length=28
+
+else
+#' Extract count on manually selected area
+#' A jason file is generated in Loupe Browser on the selected area
+#' Image should be in either TIFF, QPTIFF or JPEG format  
+
+spaceranger count --id=$library --transcriptome=$path/refdata-gex-GRCh38-2020-A --fastqs=$path/$run/fastq/$fastq --sample=$fastq --image=$path/$run/image/$image --slide=$slide --area=$area --loupe-alignment=$path/$run/image/$json --localcores=8 --localmem=500 --no-bam
+
+fi 
+
+done < <(tail -n +2 $input)
---
+++ SpaceRanger/4_SR_aggregation.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+home=$HOME
+path=$home/OVisium/Spaceranger
+input=$path/input.csv
+cd $path/Outs
+current_date_time=$(date +'%Y%m%d')
+
+#' Perform sitecheck
+export PATH=$path/spaceranger-2.0.1:$PATH
+spaceranger sitecheck > sitecheck.txt
+
+#' Create a txt file for Rascal solutions of all samples
+aggTable=$path/aggTable_$current_date_time.csv
+printf "library_id,molecule_h5,cloupe_file,spatial_folder,patient,mutation,variant,age,sample,origin,slide,area,run,fastq\n" > $aggTable 
+
+while IFS=, read -r patient mutation variant age sample origin library slide area image	run fastq json
+do
+
+molecule_h5=$path/Outs/$library/outs/molecule_info.h5
+cloupe_file=$path/Outs/$library/outs/cloupe.cloupe
+spatial_folder=$path/Outs/$library/outs/spatial
+
+printf "$library,$molecule_h5,$cloupe_file,$spatial_folder,$patient,$mutation,$variant,$age,$sample,$origin,$slide,$area,$run,$fastq\n" >> $aggTable 
+
+done < <(tail -n +2 $input)
+
+#' Perform aggregation of multiple slices
+#' A sample sheet should be prepared accordingly as the example csv file
+#' Specifiy 'none' if no seq depth normalization during the aggregation
+spaceranger aggr --id=AGG_BRCA_18_$current_date_time --csv=$aggTable --normalize=mapped
+
+
