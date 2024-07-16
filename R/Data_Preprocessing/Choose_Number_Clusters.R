608af0009d9aa85c42decc30cb36c63452066dc8
Minilool <111468388+NyKepler@users.noreply.github.com>
2024-07-16 09:14:13 +0200

    Update and rename Choose_Number_Clusters.R to Choose_Number_Clusters.R

--- R/Data_Preprocessing/Choose_Number_Clusters.R
+++ R/Data_Preprocessing/Choose_Number_Clusters.R
@@ -4,8 +4,8 @@
 
 library(fs)
 home <- path_home()
-source(paste(home, "OVisium/manuscript/gitHub/1_library.R", sep = "/"))
-source(paste(home, "OVisium/manuscript/gitHub/2_directory.R", sep = "/"))
+source(paste(home, "OVisium/R/Help_functions/Load_Library.R", sep = "/"))
+source(paste(home, "OVisium/R/Help_functions/Create_Directory.R", sep = "/"))
 
 
 choose.clustRes <- function(data) {
