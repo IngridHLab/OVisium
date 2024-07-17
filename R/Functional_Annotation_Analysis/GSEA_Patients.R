f9ee4830c2077b632f7ad42084ef0f8b809bc6ef
Minilool <111468388+NyKepler@users.noreply.github.com>
2024-07-17 13:58:17 +0200

    Update GSEA_Patients.R

--- R/Functional_Annotation_Analysis/GSEA_Patients.R
+++ R/Functional_Annotation_Analysis/GSEA_Patients.R
@@ -4,8 +4,8 @@
 #' as well as GSEA Analysis on lastest version of hallmark geneset
 library(fs)
 home <- path_home()
-source(paste(home, "OVisium/manuscript/gitHub/1_library.R", sep = "/"))
-source(paste(home, "OVisium/manuscript/gitHub/2_directory.R", sep = "/"))
+source(paste(home, "OVisium/R/Help_functions/Load_Library.R", sep = "/"))
+source(paste(home, "OVisium/R/Help_functions/Create_Directory.R", sep = "/"))
 
 #' import all annotation files
 setwd(deg.dir)
