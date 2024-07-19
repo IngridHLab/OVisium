46c781480fa6287052bc3ba9c053e221a7663bc8
Minilool <111468388+NyKepler@users.noreply.github.com>
2024-07-19 13:18:03 +0200

    Create download.sh

---
+++ R/Deconvolution/Ecotyper/download.sh
@@ -0,0 +1,11 @@
+
+#' 2. Select the working directory and download the ecotyper github files:
+cd ./OVisium/R/Deconvolution
+git clone https://github.com/digitalcytometry/ecotyper
+
+#' apply token on cibersort website: https://cibersortx.stanford.edu/getoken.php
+#' Please note that tokens are good for 6 months from date of request, so you will need to request a new token when an existing one has expired.
+
+#' 3. Edit config yml file to config_recovery_OVisium.yml:
+#' CIBERSORTx username : minerva.li@med.lu.se
+#' CIBERSORTx token : 50781fa113b3ecf64d8061bfad6fee94 (valid until 2024-11-07 16:48:15)
